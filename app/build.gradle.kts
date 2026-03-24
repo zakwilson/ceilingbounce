a935ee4085a47f82b79de217abab4d6a6fc99b80
Zak Wilson <zak.wilson@gmail.com>
2026-03-24 19:50:04 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.zakreviews.ceilingbounce"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.3.0"
+        versionCode = 3
+        versionName = "0.3.1"
     }
 
     buildTypes {
